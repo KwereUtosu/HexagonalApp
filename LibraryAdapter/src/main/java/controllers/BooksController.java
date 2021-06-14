46fa3736f3bad5a67b811ffdf9fa38f71f244e31
Joy <kwereutosu@gmail.com>
2021-06-14 16:32:40 +0100

    Intial Commit

---
+++ LibraryAdapter/src/main/java/controllers/BooksController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import models.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import services.BookService;
+
+import java.util.List;
+
+@RestController
+public class BooksController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks(){
+        return bookService.getBooks();
+    }
+
+    @PostMapping
+    public void saveBook(Book book){
+        bookService.saveBook(book);
+    }
+}
